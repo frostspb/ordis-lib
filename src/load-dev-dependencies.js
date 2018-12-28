f416087328e04331e69d7c890bd1c362d8ea95a0
Oleg Abbakumov <o.abbakumov@westcall.spb.ru>
2018-12-28 11:57:45 +0300

    changed import to testing script

--- src/load-dev-dependencies.js
+++ src/load-dev-dependencies.js
@@ -3,9 +3,7 @@ import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
-import OrdisActionButton from './lib-components/OrdisActionButton.vue'
-import OrdisCardPage from './lib-components/OrdisCardPage.vue'
-import OrdisDatepicker from './lib-components/OrdisDatepicker.vue'
+import * as components from "./lib-components/index";
 
 
 // install vuetify and any other dependencies
@@ -14,11 +12,12 @@ Vue.use(Vuetify);
 
 // Mount custom component in div provided by vue cli dev server
 var x = new Vue({
-    render: h => h(OrdisCardPage, { props: { pageIcon: 'refresh', pageTitle: 'Test Page' } },
+    render: h => h(components.OrdisCardPage, { props: { pageIcon: 'refresh', pageTitle: 'Test Page' } },
         [
-            h(OrdisDatepicker, { props: { lbTitle: 'my data', dt: new Date().toISOString().substr(0, 10) }, slot: 'content' }),
-            h(OrdisActionButton, { props: { btnIcon: 'mail', iconColor: 'red' }, slot: 'menu' }),
-            h(OrdisActionButton, { props: { btnIcon: 'call', btnTooltip: 'call me' }, slot: 'content' }),
+            h(components.OrdisDatepicker, { props: { lbTitle: 'my data', dt: new Date().toISOString().substr(0, 10) }, slot: 'content' }),
+            h(components.OrdisActionButton, { props: { btnIcon: 'mail', iconColor: 'red' }, slot: 'menu' }),
+            h(components.OrdisActionButton, { props: { btnIcon: 'call', btnTooltip: 'call me' }, slot: 'content' }),
+            h(components.OrdisEditorWindow, { props: { windowTitle: 'Editor window' }, slot: 'content' }),
         ]
     )
 })
